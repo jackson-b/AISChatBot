acf9151505916f7f67ed1f7c67914043d48fd513
jackson-b <36705108+jackson-b@users.noreply.github.com>
2018-04-12 15:04:40 -0400

    Update JrolleyAisChatBot_ScienceOlympiad.swift

--- JrolleyAisChatBot_ScienceOlympiad.swift
+++ JrolleyAisChatBot_ScienceOlympiad.swift
@@ -5,7 +5,7 @@ struct QuestionAnswerer {
         
       
         if lowerQuestion.hasPrefix("where") && lowerQuestion.contains("science") && lowerQuestion.contains("olympiad") {
-            return "Every year Science Olympiad is at a random school near Atlanta Georgia, the meets for this CAS group take place at Atlanta International School."
+            return "Every year Science Olympiad is at a random school near Atlanta, Georgia, the meets for this CAS group take place at Atlanta International School."
             
         } else if lowerQuestion.hasPrefix("what") && lowerQuestion.contains("science") && lowerQuestion.contains("is") && lowerQuestion.contains("olympiad") {
             return "Science Olympiad is an organization that brings multiple schools together every year. These schools each compete in a number of scientific events and overcome challenges to earn medals and recognition. Each event requires specific knowledge and skills to succeed."
@@ -20,16 +20,16 @@ struct QuestionAnswerer {
             return "Science Olympiad is a cut club although the vast majority of people that sign up make team. There is no need to worry as each year more slots show up and all students have a chance to prove their skills."
             
         } else if lowerQuestion.contains("when") && lowerQuestion.contains("science") && lowerQuestion.contains("olympiad") {
-            return "Science Olympiad has meets on fridays after school from 4:00 to 4:45, every year the competition takes place sometime on a saturday in february from dawn to dusk."
+            return "Science Olympiad has meets on fridays after school from 4:00 to 4:45, every year the competition takes place sometime on a saturday in February from dawn to dusk."
             
         } else if lowerQuestion.contains("what") && lowerQuestion.contains("hardest") && lowerQuestion.contains("activity") || lowerQuestion.contains("what") && lowerQuestion.contains("hardest") && lowerQuestion.contains("event")  {
             return "Hovercraft is considered the hardest activity in Science Olympiad. This is because it is time consuming, and requires a lot of knowledge to do."
             
         } else if lowerQuestion.contains("what") && lowerQuestion.hasPrefix("events") || lowerQuestion.contains("what") && lowerQuestion.contains("are") && lowerQuestion.hasPrefix("events") {
-            return "In 2018, all the event for Science Olympiad were: Anatomy and Physiology, Disease, Detectives, Ecology, Herpetology, Microbe Mission, Astronomy, Dynamic Planet, Remote Sensing, Rocks and Minerals, Chemistry Lab, Forensics, Hovercraft, Materials Science, Optics, Thermodynamics, Helicopters, Mission Possible, Mousetrap Vehicle, Towers, Experimental Design, Fermi Questions, Game On and Write It Do It."
+            return "In 2018, all the events for Science Olympiad were: Anatomy and Physiology, Disease, Detectives, Ecology, Herpetology, Microbe Mission, Astronomy, Dynamic Planet, Remote Sensing, Rocks and Minerals, Chemistry Lab, Forensics, Hovercraft, Materials Science, Optics, Thermodynamics, Helicopters, Mission Possible, Mousetrap Vehicle, Towers, Experimental Design, Fermi Questions, Game On and Write It Do It."
             
         } else if lowerQuestion.hasPrefix("how") && lowerQuestion.contains("many") && lowerQuestion.contains("events") || lowerQuestion.hasPrefix("how") && lowerQuestion.contains("many") && lowerQuestion.contains("activities") {
-            return "There are a total of 27 different event is Science Olympiad such as Towers, Wright stuff, Solar Systems, Thermodynamics and Write it do it. The first event and my personal favorite is towers. The goal is to build a tower and see how much weight it can hold. In Wright stuff you make a plan and see how long it can fly. Solar systems is a study event in which you take a test. My second favorite is Write it do it because you and your partner sit in separate rooms and one is given a construction, they then write instruction on how to make it. Next the instructions are given to the other partner and they are to rebuild the structure. Finally thermodynamics is a lab event in which you design an insulation device and test it’s efficiency while taking a test."
+            return "There are a total of 27 different event is Science Olympiad such as Towers, Wright Stuff, Solar Systems, Thermodynamics and Write it do it. The first event and my personal favorite is towers. The goal is to build a tower and see how much weight it can hold. In Wright stuff you make a plan and see how long it can fly. Solar systems is a study event in which you take a test. My second favorite is Write it do it because you and your partner sit in separate rooms and one is given a construction, they then write instruction on how to make it. Next the instructions are given to the other partner and they are to rebuild the structure. Finally thermodynamics is a lab event in which you design an insulation device and test it’s efficiency while taking a test."
             
         } else if lowerQuestion.hasPrefix("what") && lowerQuestion.contains("event") && lowerQuestion.contains("favorite") {
             return "It’s hard to choose the best event because of all the events are fun but my personal favorite is towers. The goal of this event is to build a light wood tower that is 50 cm high. You then see how much weight it can hold by putting a block on the to of the tower attached to a chain that goes down the middle of the tower. Next the chain is attached to a bucket and sand is poured in until the tower breaks. You still need to be careful because the score is measured by how much the tower can hold divided by the weight of the tower. This means the lighter your tower the better the score."
@@ -41,7 +41,7 @@ struct QuestionAnswerer {
             return "In order to win a medal, you need to win or place within the top 4 schools in an event. Even if you don’t, you still get a ribbon."
             
         } else if lowerQuestion.hasPrefix("what") && lowerQuestion.contains("win") && lowerQuestion.contains("do")  {
-            return "When you win an event or place 4th or higher you get a medal. It can be either a gold, silver bronze or crystyl color medal. Each year the school with the most medals receives a big trophy and the next 2 runner ups get smaller ones."
+            return "When you win an event or place 4th or higher you get a medal. It can be either a gold, silver, bronze or crystal color medal. Each year the school with the most medals receives a big trophy and the next 2 runner ups get smaller ones."
             
         } else if lowerQuestion.hasPrefix("what") && lowerQuestion.contains("subjects") && lowerQuestion.contains("science") && lowerQuestion.contains("olympiad") || lowerQuestion.hasPrefix("what") && lowerQuestion.contains("categories") && lowerQuestion.contains("science") && lowerQuestion.contains("olympiad")  {
             return "All events are within the categories of biology, chemistry, physics, engineering, or general knowledge."
